1f5b0c1ddf27615073183dd4f37eefc8e781ae85
hatab002 <hatab002@umn.edu>
2018-03-15 00:34:35 -0500

    javaScript in the works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,23 +2,34 @@ $(document).ready(function(){
 
 
     
-    var q1 = {
+    // var questions = [q1.question, q2.question, q3.question, q4.question, q5.question, q6.question];
+    // var randomQuestion = questions[Math.floor(Math.random() * questions.length)]
+    // $('#randomQuestion').text(randomQuestion)
+    
+    //shuffle questions
+    //for loop
+    //combine answers
+    //for loop answers
+    
+    
+    
+    
+var q1 = {
         question : "this is a random question 1",
         correctAnswer : ["correct answer"],
         wrongAnswers : ["wrong answer1", "wrong answer2", "wrong answer 3"],
-        
     }
-    var q2 = {
-        question : "this is a random question 2",
-        correctAnswer : ["correct answer"],
-        wrongAnswers : ["2wrong answer1", "2wrong answer2", "2wrong answer 3"]
-    }
-    var q3 = {
-        question : "this is a random question 3",
-        correctAnswer : ["correct answer"],
-        wrongAnswers : ["3wrong answer1", "3wrong answer2", "3wrong answer 3"]
-    }
-    var q4 = {
+var q2 = {
+    question : "this is a random question 2",
+    correctAnswer : ["correct answer"],
+    wrongAnswers : ["2wrong answer1", "2wrong answer2", "2wrong answer 3"]
+}
+var q3 = {
+    question : "this is a random question 3",
+    correctAnswer : ["correct answer"],
+    wrongAnswers : ["3wrong answer1", "3wrong answer2", "3wrong answer 3"]
+}
+var q4 = {
         question : "this is a random question 4",
         correctAnswer : ["correct answer"],
         wrongAnswers : ["4wrong answer1", "4wrong answer2", "4wrong answer 3"]
@@ -35,18 +46,40 @@ $(document).ready(function(){
     }
     
     var questions = [q1.question, q2.question, q3.question, q4.question, q5.question, q6.question];
-    var randomQuestion = questions[Math.floor(Math.random() * questions.length)]
-    $('#randomQuestion').text(randomQuestion);
-
+    
     $('#startButton').on("click", function startFunction(){
         $('#start').hide();
         $('#questions').show();
     })
+    var allObjects= [q1, q2, q3 , q4, q5, q6]
+    // function randomQuestions(){
+    //     var random = all[Math.floor(Math.random() * all.length)]
+    //     console.log(random);}
+    // randomQuestions();
+
     
+    function shuffle(array) {
+        var currentIndex = array.length, tempValue, randomIndex;
+      
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+      
+          // And swap it with the current element.
+          tempValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = tempValue;
+        }
+      
+        return array;
+      }
+      
+      console.log(shuffle(allObjects));
+      
 
 
-
-
-
-
+    
 });
